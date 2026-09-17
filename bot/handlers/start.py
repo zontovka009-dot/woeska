31ad4f9169ae75656b9460f22066ed2c0f214f39
zontovka009-dot <zontovka009@gmail.com>
2026-09-18 02:32:38 +0300

    Delete bot directory

--- bot/handlers/start.py
+++
@@ -1,7 +0,0 @@
-from aiogram import Router
-from aiogram.filters import CommandStart
-from aiogram.types import Message
-from keyboards.main import main_keyboard
-router=Router()
-@router.message(CommandStart())
-async def start(message:Message): await message.answer("🎬 <b>Wathis</b>\nСмотри видео синхронно с друзьями.",reply_markup=main_keyboard(),parse_mode="HTML")
