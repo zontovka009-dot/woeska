f7a4670d9979435f10a57e132635500f696bbfbd
zontovka009-dot <zontovka009@gmail.com>
2026-09-18 03:04:55 +0300

    Delete backend directory

--- backend/app/models/__init__.py
+++
@@ -1,6 +0,0 @@
-from .user import User
-from .session import Session
-from .friendship import Friendship
-from .message import Message
-from .invitation import Invitation
-from .session_membership import Membership
--- backend/run.py
+++
@@ -1,4 +0,0 @@
-import os
-import uvicorn
-if __name__ == "__main__":
-    uvicorn.run("app.main:app",host="0.0.0.0",port=int(os.getenv("PORT","80")))
